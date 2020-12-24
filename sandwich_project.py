64d6afe74e5af07d945e5348a93108a7d92f3234
silverchair <carlosdanielh@mail.com>
2020-12-24 15:21:01 -0400

    add while loop

--- sandwich_project.py
+++ sandwich_project.py
@@ -8,7 +8,7 @@ ask if they want mayo, mustard, lettuce, or tomato.  Using inputInt() to ask
 how many sandwiches they want. Make sure this number is 1 or more.  Come up
 with prices for each of these options, and have your program display a total
 cost after the user enters their selection.'''
-
+from pyinputplus import inputStr, inputInt, inputMenu
 items = {
     'bread': {
         'wheat': 10,
@@ -29,4 +29,16 @@ items = {
         'tomato': 3}
 }
 
-print(items['bread']['wheat'])
+
+want_new_other = True
+while want_new_other:
+    print('welcome to bread prepare yourself!!')
+    quantity_sandwich = inputInt('How many sandwich do you want: ', min=1)
+
+    has_prepared_all_sandwich = False
+    while not has_prepared_all_sandwich:
+        type_bread = inputMenu(['wheat', 'white', 'sourbread'],
+                               prompt='choose one please')
+        quantity_sandwich -= 1
+        if quantity_sandwich == 0:
+            has_prepared_all_sandwich = True
